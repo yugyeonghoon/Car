f7045be01d758abddafe83eaec26ef11ed3d9f2b
kkk <yuh3298@naver.com>
2025-04-23 17:52:11 +0900

    car dao 1차

---
+++ Car/src/main/java/carInfo/CarDAO.java
@@ -0,0 +1,34 @@
+package carInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import db.DBManager;
+
+public class CarDAO extends DBManager{
+	//차 조회
+	public List<CarVO> carView() {
+		driverLoad();
+		DBConnect();
+		
+		String sql = "select * from car_merged_result where image != '자료없음' order by rand() limit 30 ";
+		
+		executeQuery(sql);
+		
+		List<CarVO> list = new ArrayList<>();
+		while(next()) {
+			String company = getString("company");
+			String title = getString("title");
+			String img = getString("image");
+			
+			CarVO vo = new CarVO();
+			vo.setCompany(company);
+			vo.setTitle(title);
+			vo.setImage(img);
+			
+			list.add(vo);
+		}
+		DBDisConnect();
+		return list;
+	}
+}
---
+++ Car/src/main/java/carInfo/CarVO.java
@@ -0,0 +1,129 @@
+package carInfo;
+
+public class CarVO {
+	private String company;
+	private String title;
+	private String trim;
+	private String engine;
+	private String compressor;
+	private String exhaust;
+	private String gas;
+	private String output;
+	private String torque;
+	private String fuel;
+	private String length_width;
+	private String weidgt;
+	private String shift;
+	private String car_type;
+	private int year;
+	private String image;
+	private String price;
+	
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTrim() {
+		return trim;
+	}
+	public void setTrim(String trim) {
+		this.trim = trim;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public String getCompressor() {
+		return compressor;
+	}
+	public void setCompressor(String compressor) {
+		this.compressor = compressor;
+	}
+	public String getExhaust() {
+		return exhaust;
+	}
+	public void setExhaust(String exhaust) {
+		this.exhaust = exhaust;
+	}
+	public String getGas() {
+		return gas;
+	}
+	public void setGas(String gas) {
+		this.gas = gas;
+	}
+	public String getOutput() {
+		return output;
+	}
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	public String getTorque() {
+		return torque;
+	}
+	public void setTorque(String torque) {
+		this.torque = torque;
+	}
+	public String getFuel() {
+		return fuel;
+	}
+	public void setFuel(String fuel) {
+		this.fuel = fuel;
+	}
+	public String getLength_width() {
+		return length_width;
+	}
+	public void setLength_width(String length_width) {
+		this.length_width = length_width;
+	}
+	public String getWeidgt() {
+		return weidgt;
+	}
+	public void setWeidgt(String weidgt) {
+		this.weidgt = weidgt;
+	}
+	public String getShift() {
+		return shift;
+	}
+	public void setShift(String shift) {
+		this.shift = shift;
+	}
+	public String getCar_type() {
+		return car_type;
+	}
+	public void setCar_type(String car_type) {
+		this.car_type = car_type;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+	
+}
