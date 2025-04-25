2b9e4f41cde6afa090a49675092fa1902a8d6e93
ykh <ykh@naver.com>
2025-04-25 16:34:53 +0900

    car detail 수정중

---
+++ Car/src/main/java/rating/RatingVO.java
@@ -0,0 +1,75 @@
+package rating;
+
+public class RatingVO {
+	private String no;
+	private String title;
+	private String rating;
+	private String ratingPeople;
+	private String drive;
+	private String price;
+	private String habitability;
+	private String quality;
+	private String design;
+	private String fuel;
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getRatingPeople() {
+		return ratingPeople;
+	}
+	public void setRatingPeople(String ratingPeople) {
+		this.ratingPeople = ratingPeople;
+	}
+	public String getDrive() {
+		return drive;
+	}
+	public void setDrive(String drive) {
+		this.drive = drive;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getHabitability() {
+		return habitability;
+	}
+	public void setHabitability(String habitability) {
+		this.habitability = habitability;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+	public String getDesign() {
+		return design;
+	}
+	public void setDesign(String design) {
+		this.design = design;
+	}
+	public String getFuel() {
+		return fuel;
+	}
+	public void setFuel(String fuel) {
+		this.fuel = fuel;
+	}
+	
+}
